723188030ddb153fc039b6c34b60ce543c13ae40
ixwelx <ridiixwelx@gmail.com>
2025-08-26 16:10:43 +0600

    add

---
+++ main.js
@@ -0,0 +1,69 @@
+const createBtn = document.querySelector(".create-btn");
+const list = document.querySelector(".todo-list");
+const input = document.querySelector("input");
+
+let task = JSON.parse(localStorage.getItem("task"));
+
+function showTasks() {
+    list.innerHTML = "";
+    task.forEach((item, i) => {
+        const div = document.createElement("div");
+        div.setAttribute("class", "todo-item")
+
+        const btnsDiv = document.createElement("div");
+        btnsDiv.setAttribute("class", "div-btns");
+
+        const h3 = document.createElement("h3");
+        h3.innerText = item.title;
+
+        const editBtn = document.createElement("button");
+        editBtn.innerText = "Edit";
+        editBtn.setAttribute("class", "edit-btn")
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.innerText = "Delete";
+        deleteBtn.setAttribute("class", "delete-btn");
+
+        editBtn.onclick = () => editTask(i);
+        deleteBtn.onclick = () => deleteTask(i);
+
+        div.append(h3, btnsDiv );
+        btnsDiv.append(editBtn, deleteBtn);
+        list.append(div);
+    });
+
+    localStorage.setItem("task", JSON.stringify(task));
+}
+
+function createTask() {
+    if (!input.value.trim()) return alert("Введите задачу");
+
+    task.push({ title: input.value });
+    input.value = "";
+
+    showTasks();
+}
+
+function editTask(i) {
+    const newTitle = prompt("Измените задачу:");
+    if (newTitle.trim()) {
+        task[i].title = newTitle.trim();
+        showTasks();
+    }
+}
+
+function deleteTask(i) {
+    task.splice(i, 1);
+    showTasks();
+}
+
+createBtn.onclick = createTask;
+input.addEventListener("keydown", e => {
+    if (e.key === "Enter") createTask();
+});
+
+showTasks();
+
+
+
+
